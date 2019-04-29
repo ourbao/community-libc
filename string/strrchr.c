31ed448b07d465791df0e58206f323b0f5ce5e6e
James Buren <ryuo@ryuo.xyz>
2019-04-29 06:05:01 -0500

    string: add strrchr

--- string/strrchr.c
+++ string/strrchr.c
@@ -1,12 +1,11 @@
 #include "string.h"
 
-char *strchr(const char *s, int c)
+char *strrchr(const char *s, int c)
 {
 	char *p = NULL;
 	for (size_t i = 0; s[i] != '\0'; ++i) {
 		if (s[i] == c) {
 			p = (char *)&s[i];
-			break;
 		}
 	}
 	return p;
